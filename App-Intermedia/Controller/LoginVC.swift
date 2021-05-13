95a38f00c15c923f3bd266378efdba84e824bf30
Rodrigo <dev.rodrigotorres@gmail.com>
2021-05-13 11:04:54 -0300

    Added Login Screen

---
+++ App-Intermedia/Controller/LoginVC.swift
@@ -0,0 +1,40 @@
+//
+//  LoginVC.swift
+//  App-Intermedia
+//
+//  Created by Rodrigo Torres on 12/05/2021.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var emailTxtFld: UITextField!
+    @IBOutlet weak var passwordTxtFld: UITextField!
+    @IBOutlet weak var btnLogin: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        
+    }
+    
+    @IBAction func btnLoginPressed(_ sender: Any) {
+        
+    }
+    
+    func setupUI() {
+        btnLogin.layer.cornerRadius = 5
+    }
+    
+    @IBAction func editEmailTxtFld(_ sender: UITextField) {
+        sender.validateTxtFld(txtFld: sender, secondTxtFld: passwordTxtFld, button: btnLogin)
+    }
+    
+    @IBAction func editPasswordTxtFld(_ sender: UITextField) {
+        sender.validateTxtFld(txtFld: sender, secondTxtFld: emailTxtFld, button: btnLogin)
+    }
+}
